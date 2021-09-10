4f311acfaa5f335aebdff71f2ec6e0b4bca5bfad
shubhshaadivivah21 <shubhshaadivivah21@gmail.com>
2021-09-10 08:53:23 +0530

    code

--- shubhshaadivivah/settings.py
+++ shubhshaadivivah/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'ijo8611y6x@3b-+(k&5z=xj$u6olrwga^b#jp_syw+6fuelx^8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://shubhshaadivivah.herokuapp.com/', '127.0.0.1']
 
