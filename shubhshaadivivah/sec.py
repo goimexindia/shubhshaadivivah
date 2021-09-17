a3fce7c6bc68cf9a9b45d2e5a4673c55421263eb
shubhshaadivivah21 <shubhshaadivivah21@gmail.com>
2021-09-17 21:17:14 +0530

    new

---
+++ shubhshaadivivah/sec.py
@@ -0,0 +1,6 @@
+
+
+EMAIL_HOST_USER = 'shubhshaadivivah21@gmail.com'
+EMAIL_HOST_PASSWORD = 'llflzxxlbdvmacss'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
--- vivah/views.py
+++ vivah/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+
+from shubhshaadivivah import settings
 
 
 def home(request):
-    return render(request, 'index.html', {})
+    return render(request, 'vivah/index.html', {})
+
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        subject = "Thank you for contacting ShubhShaadiVivah.com."
+        message = "Dear " + name + ",\n\n" \
+                  + "We will get in touch with you soon." + "\n\n\n" \
+                  + "Your message details : -" + "\n" \
+                  + "Message From-" + name + "\n" \
+                  + "Email:-" + email + "\n\n" \
+                  + "Mobile:-" + phone + "\n\n" \
+                  + "Subject-" + subject + "\n\n" \
+                  + "Details-" + message + "\n\n\n" \
+                  + "Warm Regards \n\n From: ShubhShaadiVivah Support Team"
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [email, settings.EMAIL_HOST_USER]
+        send_mail(subject, message, from_email, to_list, fail_silently=True)
+
+
+
+        return render(request, 'vivah/contact.html', {'name': name, 'email': email})
+
+    else:
+        return render(request, 'vivah/contact.html', {})
