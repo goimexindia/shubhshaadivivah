df9f68cf0d7a10aba9495f43038a2a7dcaecabdb
shubhshaadivivah21 <shubhshaadivivah21@gmail.com>
2021-10-01 19:05:36 +0530

    login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from .views import *
 
 urlpatterns = [
     path('register', views.register, name="register"),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate
@@ -5,6 +6,9 @@ from django.core.mail import send_mail
 from shubhshaadivivah import settings
 from vivah.models import Contactme
 from django.contrib.auth.models import User, auth
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView
+from accounts.forms import *
 
 
 def register(request):
@@ -31,3 +35,18 @@ def logout(request):
     auth.logout(request)
     messages.info(request, "Logged OUT successfully!")
     return redirect('/')
+
+
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/signup.html'
+
+
+class ProfileView(SuccessMessageMixin,  UpdateView):
+    model = User
+    form_class = ProfileForm
+    success_url = reverse_lazy('home')
+    template_name = 'accounts/profile.html'
+    success_message = 'PROFILE successfully saved!!!!'
+
