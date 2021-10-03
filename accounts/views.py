f3cfac4771cfa4be61d9e7a8c195cddbf9876b81
shubhshaadivivah21 <shubhshaadivivah21@gmail.com>
2021-10-03 09:15:41 +0530

    new

--- accounts/views.py
+++ accounts/views.py
@@ -87,7 +87,7 @@ def activate(request, uidb64, token):
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
-        login(request, user)
+        login(request)
         # return redirect('home')
         return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
     else:
