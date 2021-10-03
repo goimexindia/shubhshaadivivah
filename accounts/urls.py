2defeeb371c792bc2c16dac13dc305c6399f8429
shubhshaadivivah21 <shubhshaadivivah21@gmail.com>
2021-10-03 07:48:04 +0530

    new

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
     path('signup/', views.signup, name='signup'),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
     path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
     path('social-auth/', include('social_django.urls', namespace="social")),
