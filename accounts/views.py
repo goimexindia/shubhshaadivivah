c5f9170a1f6d830b9438f92a1579e0cb1b74b3d6
shubhshaadivivah21 <shubhshaadivivah21@gmail.com>
2021-10-03 06:50:28 +0530

    new

--- accounts/views.py
+++ accounts/views.py
@@ -62,6 +62,7 @@ def signup(request):
             message = render_to_string('accounts/acc_active_email.html', {
                 'user': user,
                 'domain': 'https://shubhshaadivivah.herokuapp.com/',
+                'domain': current_site.domain,
                 'uid': urlsafe_base64_encode(force_bytes(user.pk)),
                 'token': account_activation_token.make_token(user),
             })
