213bdffdef64f79af4b4ac0c22cf0440321674d6
shubhshaadivivah21 <shubhshaadivivah21@gmail.com>
2021-10-09 12:24:26 +0530

    new

--- shubhshaadivivah/settings.py
+++ shubhshaadivivah/settings.py
@@ -97,10 +97,19 @@ WSGI_APPLICATION = 'shubhshaadivivah.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
     }
 }
 
