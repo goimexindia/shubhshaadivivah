4af92f6ad5a6faf5bb75f92fe36eefc1ff4e66a4
shubhshaadivivah21 <shubhshaadivivah21@gmail.com>
2021-10-09 14:27:45 +0530

    new

--- shubhshaadivivah/settings.py
+++ shubhshaadivivah/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'ijo8611y6x@3b-+(k&5z=xj$u6olrwga^b#jp_syw+6fuelx^8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
