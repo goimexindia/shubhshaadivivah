ebad65fc730ddda6d144ec31456818c991da1e3c
shubhshaadivivah21 <shubhshaadivivah21@gmail.com>
2021-10-11 23:35:52 +0530

    new

--- accounts/models.py
+++ accounts/models.py
@@ -213,7 +213,6 @@ class Whatyouwant(models.Model):
 
 
 class ColdCoffe(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     amount = models.CharField(max_length=100)
     order_id = models.CharField(max_length=100, blank=True)
