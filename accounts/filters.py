5738c4c390d8ab91e824b13199f53c850564702c
shubhshaadivivah21 <shubhshaadivivah21@gmail.com>
2021-11-11 19:43:30 +0530

    new

--- accounts/filters.py
+++ accounts/filters.py
@@ -7,11 +7,11 @@ class OrderFilter(django_filters.FilterSet):
     city = CharFilter(field_name='city', lookup_expr='icontains')
     start_age = CharFilter(field_name="age", lookup_expr='gte')
     end_age = CharFilter(field_name="age", lookup_expr='lte')
-
+    user_id = CharFilter(field_name="id", lookup_expr='iexact')
 
     class Meta:
         model = Profile
-        fields = ('languages', 'caste', 'religion', 'education','state',)
+        fields = ('languages', 'caste', 'religion', 'education', 'state',)
 
         exclude = ['email',
                    'birthday', 'birthtimehh', 'birthtimemm', 'birthplace', 'birthstate', 'birthcountry', 'ampm',
