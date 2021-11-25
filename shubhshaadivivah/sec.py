ebd8564de0899554fc93e01ea9c022a3350a9e0a
shubhshaadivivah21 <shubhshaadivivah21@gmail.com>
2021-11-25 17:11:41 +0530

    new

--- shubhshaadivivah/sec.py
+++ shubhshaadivivah/sec.py
@@ -1,6 +1,6 @@
 
 
 EMAIL_HOST_USER = 'shubhshaadivivah21@gmail.com'
-EMAIL_HOST_PASSWORD = 'llflzxxlbdvmacss'
+EMAIL_HOST_PASSWORD = 'btdtqobfzboiujiy'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
